b16a5265c40dbb0f0af1588571b60b75a444ab12
Daniel-H-99 <myhwang99@kaist.ac.kr>
2024-01-18 00:35:07 +0900

    refined_training

--- src/server/feddiff.py
+++ src/server/feddiff.py
@@ -221,12 +221,12 @@ def get_feddiff_argparser() -> ArgumentParser:
     parser.add_argument("--save_model", type=int, default=0)
     parser.add_argument("--save_fig", type=int, default=1)
     parser.add_argument("--save_metrics", type=int, default=1)
-    parser.add_argument("--save_gap", type=int, default=1)
+    parser.add_argument("--save_gap", type=int, default=5)
     parser.add_argument("--viz_win_name", type=str, required=False)
     parser.add_argument("-cfg", "--config_file", type=str, default="")
     parser.add_argument("--check_convergence", type=int, default=1)
     parser.add_argument("--personal_tag", type=str, default=None)
-    parser.add_argument("--ckpt", type=str, default='/home/server36/minyeong_workspace/FL-bench/out_cifar10_niid3_lvqfed_trial1/FedDiff/checkpoints/cifar10_niid3_64_custom')
+    parser.add_argument("--ckpt", type=str, default=None)
     return parser
 
 
@@ -346,7 +346,7 @@ class FedDiffServer:
             for _ in range(self.args.global_epoch)
         ]
         self.selected_clients: List[int] = []
-        self.current_epoch = 64
+        self.current_epoch = 0
         # For controlling behaviors of some specific methods while testing (not used by all methods)
         self.test_flag = False
 
