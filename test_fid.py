665c1379fa0078ade3dee835a20190f7c367148f
Daniel-H-99 <myhwang99@kaist.ac.kr>
2024-01-18 20:14:19 +0900

    refined

--- test_fid.py
+++ test_fid.py
@@ -15,7 +15,7 @@ sys.path.append(Path(__file__).parent.joinpath("src/server").absolute().as_posix
 
 # image_fid_dir = '/home/server36/minyeong_workspace/FL-bench/images_fid'
 # image_fid_dir = '/home/server36/minyeong_workspace/FL-bench/tmp_phoenix'
-image_fid_dir = '/home/server36/minyeong_workspace/FL-bench/out_cifar10_niid3_phoenix_trial1/FedDiff/images_fid'
+image_fid_dir = '/home/server36/minyeong_workspace/FL-bench/out_cifar10_niid3_fed_trial1/FedDiff/images_fid'
 true_image_dir = '/home/server36/minyeong_workspace/FL-bench/data/cifar10_niid3/raw'
 
 CID=0
@@ -187,7 +187,7 @@ def main():
     
     # print(f'loaded server')
     
-    ckpt_dir = f'/home/server36/minyeong_workspace/FL-bench/out_cifar10_niid3_phoenix_trial1/FedDiff/checkpoints'
+    ckpt_dir = f'/home/server36/minyeong_workspace/FL-bench/out_cifar10_niid3_fed_trial1/FedDiff/checkpoints'
     files = sorted(list(set([int(f.split('_')[2]) for f in os.listdir(ckpt_dir) ])))
     ckpt_name_list = [os.path.join(ckpt_dir, f"cifar10_niid3_{f}_custom") for f in files]
     
@@ -214,7 +214,7 @@ def main():
 
     
     fid_dict = calc_fid_dict(ckpt_name_list)
-    with open(f'tested_fid_phoenix_cifar10_niid3_client_{CID}.pkl', 'wb') as f:
+    with open(f'tested_fid_fed_cifar10_niid3_client_{CID}.pkl', 'wb') as f:
         pkl.dump(fid_dict, f)
 
         
